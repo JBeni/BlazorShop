c9aea880968e914852546f4177f5cc5d80a31524
JBeni <JBeni@users.noreply.github.com>
2021-12-31 17:11:09 +0200

    create category handler commands

---
+++ BlazorShop.Application/Handlers/Commands/CategoryCommand/CreateCategoryCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿namespace BlazorShop.Application.Handlers.Commands.CategoryCommand
+{
+    public class CreateCategoryCommandHandler : IRequestHandler<CreateCategoryCommand, RequestResponse>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        public CreateCategoryCommandHandler(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<RequestResponse> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var entity = new Category
+                {
+                    Name = request.Name
+                };
+
+                _dbContext.Categories.Add(entity);
+                await _dbContext.SaveChangesAsync(cancellationToken);
+                return RequestResponse.Success();
+            }
+            catch (Exception ex)
+            {
+                return RequestResponse.Error(new Exception("There was an error creating the category", ex));
+            }
+        }
+    }
+}
---
+++ BlazorShop.Application/Handlers/Commands/CategoryCommand/DeleteCategoryCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace BlazorShop.Application.Handlers.Commands.CategoryCommand
+{
+    public class DeleteCategoryCommandHandler : IRequestHandler<DeleteCategoryCommand, RequestResponse>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        public DeleteCategoryCommandHandler(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<RequestResponse> Handle(DeleteCategoryCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var entity = _dbContext.Categories.SingleOrDefault(d => d.Id == request.Id);
+
+                _dbContext.Categories.Update(entity);
+                await _dbContext.SaveChangesAsync(cancellationToken);
+                return RequestResponse.Success();
+            }
+            catch (Exception ex)
+            {
+                return RequestResponse.Error(new Exception("There was an error deleting the category", ex));
+            }
+        }
+    }
+}
