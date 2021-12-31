3a1ef4f5c1484e61e77092f8fa35d73bd64bd67f
JBeni <JBeni@users.noreply.github.com>
2021-12-31 17:20:02 +0200

    create seed model

---
+++ BlazorShop.Infrastructure/Utils/SeedDataModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorShop.Infrastructure.Utils
+{
+    public class SeedDataModel
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? AdminPassword { get; set; }
+        public string? AdminRoleName { get; set; }
+        public string? AdminRoleNormalized { get; set; }
+        public string? DefaultRoleName { get; set; }
+        public string? DefaultRoleNormalized { get; set; }
+    }
+}
