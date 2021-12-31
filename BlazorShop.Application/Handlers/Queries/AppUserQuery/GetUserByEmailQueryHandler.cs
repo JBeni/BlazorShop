2fe8f0518fc0f34ce212d07922239896958c5ca3
JBeni <JBeni@users.noreply.github.com>
2021-12-31 17:12:08 +0200

    create user handler queries

---
+++ BlazorShop.Application/Handlers/Queries/AppUserQuery/GetUserByEmailQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace BlazorShop.Application.Handlers.Queries.AppUserQuery
+{
+    public class GetUserByEmailQueryHandler : IRequestHandler<GetUserByEmailQuery, AppUserResponse>
+    {
+        private readonly IAppUserService _AppUserService;
+
+        public GetUserByEmailQueryHandler(IAppUserService AppUserService)
+        {
+            _AppUserService = AppUserService;
+        }
+
+        public Task<AppUserResponse> Handle(GetUserByEmailQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return Task.FromResult(_AppUserService.GetUserByEmail(request));
+            }
+            catch (Exception ex)
+            {
+                return Task.FromResult(new AppUserResponse
+                {
+                    Error = "There was an error getting the user by email... " + ex.Message ?? ex.InnerException.Message
+                });
+            }
+        }
+    }
+}
