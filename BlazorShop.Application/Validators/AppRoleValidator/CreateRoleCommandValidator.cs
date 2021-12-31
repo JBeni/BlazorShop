25d9234107ed5a7d77a17a6386863890f90dd42f
JBeni <JBeni@users.noreply.github.com>
2021-12-31 17:15:00 +0200

    create role validators

---
+++ BlazorShop.Application/Validators/AppRoleValidator/CreateRoleCommandValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorShop.Application.Validators.AppRoleValidator
+{
+    public class CreateRoleCommandValidator : AbstractValidator<CreateRoleCommand>
+    {
+        public CreateRoleCommandValidator()
+        {
+            RuleFor(v => v.Name).NotEmpty().NotNull();
+        }
+    }
+}
---
+++ BlazorShop.Application/Validators/AppRoleValidator/DeleteRoleCommandValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorShop.Application.Validators.AppRoleValidator
+{
+    public class DeleteRoleCommandValidator : AbstractValidator<DeleteRoleCommand>
+    {
+        public DeleteRoleCommandValidator()
+        {
+            RuleFor(v => v.Id).NotEmpty().NotNull().GreaterThan(0);
+        }
+    }
+}
