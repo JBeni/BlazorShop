a95b81bf7176f1ea9dc84be3f800aff7466503c7
JBeni <JBeni@users.noreply.github.com>
2021-12-31 17:11:36 +0200

    create product photo handler commands

---
+++ BlazorShop.Application/Handlers/Commands/ProductPhotoCommand/CreateProductPhotoCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿namespace BlazorShop.Application.Handlers.Commands.ProductPhotoCommand
+{
+    public class CreateProductPhotoCommandHandler : IRequestHandler<CreateProductPhotoCommand, RequestResponse>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        public CreateProductPhotoCommandHandler(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<RequestResponse> Handle(CreateProductPhotoCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var entity = new ProductPhoto
+                {
+                    RelativePathImage = request.Image
+                };
+
+                _dbContext.ProductPhotos.Add(entity);
+                await _dbContext.SaveChangesAsync(cancellationToken);
+                return RequestResponse.Success();
+            }
+            catch (Exception ex)
+            {
+                return RequestResponse.Error(new Exception("There was an error creating the product photo", ex));
+            }
+        }
+    }
+}
---
+++ BlazorShop.Application/Handlers/Commands/ProductPhotoCommand/UpdateProductPhotoCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace BlazorShop.Application.Handlers.Commands.ProductPhotoCommand
+{
+    public class UpdateProductPhotoCommandHandler : IRequestHandler<UpdateProductPhotoCommand, RequestResponse>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        public UpdateProductPhotoCommandHandler(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<RequestResponse> Handle(UpdateProductPhotoCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var entity = _dbContext.ProductPhotos.SingleOrDefault(d => d.Id == request.Id);
+                entity.RelativePathImage = request.Image;
+
+                _dbContext.ProductPhotos.Update(entity);
+                await _dbContext.SaveChangesAsync(cancellationToken);
+                return RequestResponse.Success();
+            }
+            catch (Exception ex)
+            {
+                return RequestResponse.Error(new Exception("There was an error updating the product photo", ex));
+            }
+        }
+    }
+}
