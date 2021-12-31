a850761d515154a5c617d51b6f5523f16a788d38
JBeni <JBeni@users.noreply.github.com>
2021-12-31 17:11:16 +0200

    create order handler commands

---
+++ BlazorShop.Application/Handlers/Commands/OrderCommand/DeleteOrderCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace BlazorShop.Application.Handlers.Commands.OrderCommand
+{
+    public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand, RequestResponse>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        public DeleteOrderCommandHandler(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<RequestResponse> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var entity = _dbContext.Orders.SingleOrDefault(d => d.Id == request.Id);
+
+                _dbContext.Orders.Remove(entity);
+                await _dbContext.SaveChangesAsync(cancellationToken);
+                return RequestResponse.Success();
+            }
+            catch (Exception ex)
+            {
+                return RequestResponse.Error(new Exception("There was an error deleting the order", ex));
+            }
+        }
+    }
+}
---
+++ BlazorShop.Application/Handlers/Commands/OrderCommand/UpdateOrderCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace BlazorShop.Application.Handlers.Commands.OrderCommand
+{
+    public class UpdateOrderCommandHandler : IRequestHandler<UpdateOrderCommand, RequestResponse>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        public UpdateOrderCommandHandler(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<RequestResponse> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var entity = _dbContext.Orders.SingleOrDefault(d => d.Id == request.Id);
+                entity.Quantity = request.Quantity;
+
+                _dbContext.Orders.Update(entity);
+                await _dbContext.SaveChangesAsync(cancellationToken);
+                return RequestResponse.Success();
+            }
+            catch (Exception ex)
+            {
+                return RequestResponse.Error(new Exception("There was an error updating the order", ex));
+            }
+        }
+    }
+}
