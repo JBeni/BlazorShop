0c8b1edf6d240c10d012ce1fbf9d7be6a9eb563f
JBeni <JBeni@users.noreply.github.com>
2021-12-31 17:12:30 +0200

    create order handler queries

---
+++ BlazorShop.Application/Handlers/Queries/OrderQuery/GetOrderByIdQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿namespace BlazorShop.Application.Handlers.Queries.OrderQuery
+{
+    public class GetOrderByIdQueryHandler : IRequestHandler<GetOrderByIdQuery, OrderResponse>
+    {
+        private readonly IApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetOrderByIdQueryHandler(IApplicationDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public Task<OrderResponse> Handle(GetOrderByIdQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = _dbContext.Orders
+                    .Where(d => d.Id == request.Id)
+                    .ProjectTo<OrderResponse>(_mapper.ConfigurationProvider)
+                    .FirstOrDefault();
+                return Task.FromResult(result);
+            }
+            catch (Exception ex)
+            {
+                return Task.FromResult(new OrderResponse
+                {
+                    Error = "There was an error while getting the order... " + ex.Message ?? ex.InnerException.Message
+                });
+            }
+        }
+    }
+}
