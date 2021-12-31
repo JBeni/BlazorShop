27bbbdb9881e43cc9787b8df21ae5b51ea8ce93c
JBeni <JBeni@users.noreply.github.com>
2021-12-31 17:10:21 +0200

    create models

---
+++ BlazorShop.Application/Common/Models/EmailSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorShop.Application.Common.Models
+{
+    public class EmailSettings
+    {
+        public string? Host { get; set; }
+        public int Port { get; set; }
+        public string? Subject { get; set; }
+        public string? Message { get; set; }
+
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+    }
+}
---
+++ BlazorShop.Application/Common/Models/JwtTokenConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorShop.Application.Common.Models
+{
+    public class JwtTokenConfig
+    {
+        public string? Secret { get; set; }
+        public string? Issuer { get; set; }
+        public string? Audience { get; set; }
+        public string? AccessToken { get; set; }
+        public string? RefreshToken { get; set; }
+    }
+}
