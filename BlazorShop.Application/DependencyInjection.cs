db4e819863f958337a34e5813e66fffceb77fa3d
JBeni <JBeni@users.noreply.github.com>
2021-12-31 17:15:59 +0200

    create dependency injection - application layer

---
+++ BlazorShop.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorShop.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+            return services;
+        }
+    }
+}
