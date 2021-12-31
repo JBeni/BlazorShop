aee771221a77a23bbd3834138f490941b56a2e07
JBeni <JBeni@users.noreply.github.com>
2021-12-31 17:09:46 +0200

    create behaviours handlers

---
+++ BlazorShop.Application/Common/Behaviours/UnhandledExceptionBehaviour.cs
@@ -0,0 +1,26 @@
+﻿namespace BlazorShop.Application.Common.Behaviours
+{
+    public class UnhandledExceptionBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        public UnhandledExceptionBehaviour(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (Exception ex)
+            {
+                var requestName = typeof(TRequest).Name;
+                _logger.LogError(ex, "BlazorShop Request: Unhandled Exception for Request {Name} {@Request}", requestName, request);
+                throw;
+            }
+        }
+    }
+}
