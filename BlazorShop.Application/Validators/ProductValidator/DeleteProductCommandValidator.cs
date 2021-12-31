09f0ae02bc763b2a3c7eebbbc8936fb3fb307363
JBeni <JBeni@users.noreply.github.com>
2021-12-31 17:15:39 +0200

    create product validators

---
+++ BlazorShop.Application/Validators/ProductValidator/DeleteProductCommandValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorShop.Application.Validators.ProductValidator
+{
+    public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
+    {
+        public DeleteProductCommandValidator()
+        {
+            RuleFor(v => v.Id).NotEmpty().NotNull().GreaterThan(0);
+        }
+    }
+}
---
+++ BlazorShop.Application/Validators/ProductValidator/UpdateProductCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorShop.Application.Validators.ProductValidator
+{
+    public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+    {
+        public UpdateProductCommandValidator()
+        {
+            RuleFor(v => v.Id).NotEmpty().NotNull().GreaterThan(0);
+            RuleFor(v => v.Description).NotEmpty().NotNull();
+            RuleFor(v => v.Price).NotEmpty().NotNull().GreaterThan(0);
+        }
+    }
+}
