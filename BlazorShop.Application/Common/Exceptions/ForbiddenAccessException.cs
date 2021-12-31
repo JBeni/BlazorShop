99f0c55822f04c47e1536ccd7b81535f426c22ab
JBeni <JBeni@users.noreply.github.com>
2021-12-31 17:09:58 +0200

    create exceptions

---
+++ BlazorShop.Application/Common/Exceptions/ForbiddenAccessException.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorShop.Application.Common.Exceptions
+{
+    public class ForbiddenAccessException : Exception
+    {
+        public ForbiddenAccessException() : base() { }
+    }
+}
