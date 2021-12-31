ced109f23a75e7f0bd3b8ed1851062fe7765a0f7
JBeni <JBeni@users.noreply.github.com>
2021-12-31 17:15:21 +0200

    create order validators

---
+++ BlazorShop.Application/Validators/OrderValidator/DeleteOrderCommandValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorShop.Application.Validators.OrderValidator
+{
+    public class DeleteOrderCommandValidator : AbstractValidator<DeleteOrderCommand>
+    {
+        public DeleteOrderCommandValidator()
+        {
+            RuleFor(v => v.Id).NotEmpty().NotNull().GreaterThan(0);
+        }
+    }
+}
