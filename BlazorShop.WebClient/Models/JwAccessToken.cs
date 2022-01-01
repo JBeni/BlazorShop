5ad85140049d6f80fdbb3a442f6feacd1277aa28
JBeni <JBeni@users.noreply.github.com>
2022-01-01 22:42:20 +0200

    jwt access token model

---
+++ BlazorShop.WebClient/Models/JwAccessToken.cs
@@ -0,0 +1,9 @@
+﻿namespace Blazor.Server.Models
+{
+    public class JwAccessToken
+    {
+        public string? Access_Token { get; set; }
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+    }
+}
