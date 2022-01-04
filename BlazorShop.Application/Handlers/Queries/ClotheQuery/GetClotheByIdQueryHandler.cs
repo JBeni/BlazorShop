a1b6c84d17ad547c88cf48b7322aa2262929c072
JBeni <JBeni@users.noreply.github.com>
2022-01-04 20:12:07 +0200

    create clothe query handlers

---
+++ BlazorShop.Application/Handlers/Queries/ClotheQuery/GetClotheByIdQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿namespace BlazorShop.Application.Handlers.Queries.ClotheQuery
+{
+    public class GetClotheByIdQueryHandler : IRequestHandler<GetClotheByIdQuery, ClotheResponse>
+    {
+        private readonly IApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetClotheByIdQueryHandler(IApplicationDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public Task<ClotheResponse> Handle(GetClotheByIdQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = _dbContext.Clothes
+                    .Where(x => x.Id == request.Id)
+                    .ProjectTo<ClotheResponse>(_mapper.ConfigurationProvider)
+                    .FirstOrDefault();
+                return Task.FromResult(result);
+            }
+            catch (Exception ex)
+            {
+                return Task.FromResult(new ClotheResponse
+                {
+                    Error = "There was an error while getting the orders... " + ex.Message ?? ex.InnerException.Message
+                });
+            }
+        }
+    }
+}
---
+++ BlazorShop.Application/Handlers/Queries/ClotheQuery/GetClothesQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿namespace BlazorShop.Application.Handlers.Queries.ClotheQuery
+{
+    public class GetClothesQueryHandler : IRequestHandler<GetClothesQuery, List<ClotheResponse>>
+    {
+        private readonly IApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetClothesQueryHandler(IApplicationDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public Task<List<ClotheResponse>> Handle(GetClothesQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = _dbContext.Clothes
+                    .ProjectTo<ClotheResponse>(_mapper.ConfigurationProvider)
+                    .ToList();
+                return Task.FromResult(result);
+            }
+            catch (Exception ex)
+            {
+                List<ClotheResponse> ordersView = new();
+                ordersView.Add(new ClotheResponse
+                {
+                    Error = "There was an error while getting the orders... " + ex.Message ?? ex.InnerException.Message
+                });
+                return Task.FromResult(ordersView);
+            }
+        }
+    }
+}
