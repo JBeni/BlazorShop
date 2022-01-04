1520c4df119ea353eb37c8052796f673bd0c0d1b
JBeni <JBeni@users.noreply.github.com>
2022-01-04 20:15:24 +0200

    create services - web assembly client

---
+++ BlazorShop.WebClient/Services/CartService.cs
@@ -0,0 +1,20 @@
+﻿using static BlazorShop.WebClient.Pages.Other;
+
+namespace BlazorShop.WebClient.Services
+{
+    public class CartService
+    {
+        public void EmptyCart()
+        {
+        }
+
+        public List<CartItem> GetCartItems()
+        {
+            return new List<CartItem>();
+        }
+
+        public void DeleteItem(CartItem item)
+        {
+        }
+    }
+}
---
+++ BlazorShop.WebClient/Services/ClotheService.cs
@@ -0,0 +1,27 @@
+﻿namespace BlazorShop.WebClient.Services
+{
+    public class ClotheService
+    {
+        public List<ClotheResponse> GetClothes()
+        {
+            return new List<ClotheResponse>();
+        }
+
+        public ClotheResponse GetClothe(int id)
+        {
+            return new ClotheResponse();
+        }
+
+        public void AddClothe(ClotheResponse clothe)
+        {
+        }
+
+        public void UpdateClothe(ClotheResponse clothe)
+        {
+        }
+
+        public void DeleteClothe(int id)
+        {
+        }
+    }
+}
