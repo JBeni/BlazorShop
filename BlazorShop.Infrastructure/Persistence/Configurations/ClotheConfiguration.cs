ca930e34ada8329800ff46320d40153e12085944
JBeni <JBeni@users.noreply.github.com>
2022-01-04 20:14:27 +0200

    create clothe entity configuration

---
+++ BlazorShop.Infrastructure/Persistence/Configurations/ClotheConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorShop.Infrastructure.Persistence.Configurations
+{
+    public class ClotheConfiguration : IEntityTypeConfiguration<Clothe>
+    {
+        public void Configure(EntityTypeBuilder<Clothe> builder)
+        {
+            builder.ToTable("Clothes");
+
+            builder.Property(t => t.Name)
+                .HasMaxLength(200)
+                .IsRequired();
+        }
+    }
+}
