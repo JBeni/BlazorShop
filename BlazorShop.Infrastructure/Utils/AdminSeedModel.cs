955813abb89b109e8d0274278e6ed24d2d72ce76
JBeni <JBeni@users.noreply.github.com>
2022-01-04 20:11:16 +0200

    create seed model for roles and admin

---
+++ BlazorShop.Infrastructure/Utils/AdminSeedModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorShop.Infrastructure.Utils
+{
+    public class AdminSeedModel
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? RoleName { get; set; }
+    }
+}
---
+++ BlazorShop.Infrastructure/Utils/RolesSeedModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorShop.Infrastructure.Utils
+{
+    public class RolesSeedModel
+    {
+        public string? AdminRoleName { get; set; }
+        public string? AdminRoleNormalizedName { get; set; }
+        public string? DefaultRoleName { get; set; }
+        public string? DefaultRoleNormalizedName { get; set; }
+        public string? UserRoleName { get; set; }
+        public string? UserRoleNormalizedName { get; set; }
+    }
+}
