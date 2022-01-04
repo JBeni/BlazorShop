c0a50ba5374e92418b5f435d36b1fbe95f2cad3b
JBeni <JBeni@users.noreply.github.com>
2022-01-04 20:14:10 +0200

    update clothe and product entities

---
+++ BlazorShop.Domain/Entities/Clothe.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorShop.Domain.Entities
+{
+    public class Clothe : EntityBase
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal? Price { get; set; }
+        public int Amount { get; set; }
+        public string? ImageName { get; set; }
+        public string? ImagePath { get; set; }
+        public bool? IsActive { get; set; }
+    }
+}
--- BlazorShop.Domain/Entities/Product.cs
+++ BlazorShop.Domain/Entities/Product.cs
@@ -5,12 +5,5 @@
         public string? Name { get; set; }
         public string? Description { get; set; }
         public decimal Price { get; set; }
-
-        public int BrandId { get; set; }
-        public Brand? Brand { get; set; }
-
-        public virtual ICollection<ProductPhoto>? ProductPhotos { get; set; }
-        public virtual ICollection<Color>? Colors { get; set; }
-        public virtual ICollection<Size>? Sizes { get; set; }
     }
 }
