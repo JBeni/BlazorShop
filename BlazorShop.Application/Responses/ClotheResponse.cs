8ce134393028e72fc583a791bea26aa266233ad9
JBeni <JBeni@users.noreply.github.com>
2022-01-04 20:14:48 +0200

    create clothe response

---
+++ BlazorShop.Application/Responses/ClotheResponse.cs
@@ -0,0 +1,29 @@
+﻿namespace BlazorShop.Application.Responses
+{
+    public class ClotheResponse : IMapFrom<Clothe>
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal? Price { get; set; }
+        public int Amount { get; set; }
+        public string? ImageName { get; set; }
+        public string? ImagePath { get; set; }
+        public bool? IsActive { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Clothe, ClotheResponse>()
+                .ForMember(d => d.Id, opt => opt.MapFrom(s => s.Id))
+                .ForMember(d => d.Name, opt => opt.MapFrom(s => s.Name))
+                .ForMember(d => d.Description, opt => opt.MapFrom(s => s.Description))
+                .ForMember(d => d.Price, opt => opt.MapFrom(s => s.Price))
+                .ForMember(d => d.Amount, opt => opt.MapFrom(s => s.Amount))
+                .ForMember(d => d.ImageName, opt => opt.MapFrom(s => s.ImageName))
+                .ForMember(d => d.ImagePath, opt => opt.MapFrom(s => s.ImagePath))
+                .ForMember(d => d.IsActive, opt => opt.MapFrom(s => s.IsActive));
+        }
+
+        public string? Error { get; set; }
+    }
+}
