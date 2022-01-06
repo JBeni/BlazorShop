2d8a1394b9636f2219f26e896a3e194c1dd441a1
JBeni <JBeni@users.noreply.github.com>
2022-01-06 23:26:04 +0200

    creating policies

--- BlazorShop.WebClient/Program.cs
+++ BlazorShop.WebClient/Program.cs
@@ -1,13 +1,20 @@
 
+using Microsoft.AspNetCore.Authorization;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 builder.Services.AddBlazoredLocalStorage();
-//builder.Services.AddProtectedBrowserStorage();
-builder.Services.AddAuthorizationCore();
+builder.Services.AddAuthorizationCore(config =>
+{
+    config.AddPolicy("Admin", builder => builder.RequireClaim("Admin"));
+    config.AddPolicy("User", new AuthorizationPolicyBuilder().RequireAuthenticatedUser().RequireRole("User").Build());
+    config.AddPolicy("Default", new AuthorizationPolicyBuilder().RequireAuthenticatedUser().RequireRole("Default").Build());
+});
+
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
 
 // Inject Services
@@ -15,6 +22,6 @@ builder.Services.AddScoped<ClotheService>();
 builder.Services.AddScoped<CartService>();
 
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44351/api/") });
 
 await builder.Build().RunAsync();
