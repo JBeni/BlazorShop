3c5cd7fff1ed05ee80a68565a260db34f3445bc9
JBeni <JBeni@users.noreply.github.com>
2022-01-10 08:48:02 +0200

    create Cart entity

---
+++ BlazorShop.Domain/Entities/Cart.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorShop.Domain.Entities
+{
+    public class Cart : EntityBase
+    {
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public int Amount { get; set; }
+
+        public Clothe? Clothe { get; set; }
+    }
+}
