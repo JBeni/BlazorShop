8d67e8ce5f82f99bdc9583310d44e9e514885944
JBeni <JBeni@users.noreply.github.com>
2022-01-10 08:46:02 +0200

    create user service - blazor web

---
+++ BlazorShop.WebClient/Interfaces/IUserService.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorShop.WebClient.Interfaces
+{
+    public interface IUserService
+    {
+        Task<List<UserResponse>> GetUsers();
+        Task<UserResponse> GetUser(int id);
+        Task AddUser(UserResponse User);
+        Task UpdateUser(UserResponse User);
+        Task DeleteUser(int id);
+    }
+}
---
+++ BlazorShop.WebClient/Services/UserService.cs
@@ -0,0 +1,51 @@
+﻿namespace BlazorShop.WebClient.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly HttpClient _httpClient;
+
+        public UserService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task AddUser(UserResponse user)
+        {
+            await _httpClient.PostAsJsonAsync($"Users/user", user);
+        }
+
+        public async Task DeleteUser(int id)
+        {
+            await _httpClient.GetAsync($"Users/user/{id}");
+        }
+
+        public async Task<UserResponse> GetUser(int id)
+        {
+            var authResult = await _httpClient.GetAsync($"Users/user/{id}");
+            var authContent = await authResult.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<UserResponse>(
+                authContent,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+            );
+
+            return result;
+        }
+
+        public async Task<List<UserResponse>> GetUsers()
+        {
+            var authResult = await _httpClient.GetAsync("Users/users");
+            var authContent = await authResult.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<List<UserResponse>>(
+                authContent,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+            );
+
+            return result;
+        }
+
+        public async Task UpdateUser(UserResponse user)
+        {
+            await _httpClient.PutAsJsonAsync($"Users/user", user);
+        }
+    }
+}
