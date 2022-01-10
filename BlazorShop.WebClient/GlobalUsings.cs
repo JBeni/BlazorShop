885d8e7263200b5fafd518bd0ca09dac493a4e24
JBeni <JBeni@users.noreply.github.com>
2022-01-10 08:50:46 +0200

    add new import

--- BlazorShop.WebClient/GlobalUsings.cs
+++ BlazorShop.WebClient/GlobalUsings.cs
@@ -12,5 +12,6 @@ global using BlazorShop.Application.Commands.AccountCommand;
 global using BlazorShop.WebClient.Auth;
 global using BlazorShop.Application.Responses;
 global using BlazorShop.WebClient.Services;
+global using BlazorShop.WebClient.Interfaces;
 global using BlazorShop.WebClient.AuthPolicies;
 global using System.Net.Http.Json;
