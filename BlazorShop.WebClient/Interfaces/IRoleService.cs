7920e30e79a7c35f3fdf18a126080208a0f7b2b9
JBeni <JBeni@users.noreply.github.com>
2022-01-10 08:45:33 +0200

    create role service - blazor web

---
+++ BlazorShop.WebClient/Interfaces/IRoleService.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorShop.WebClient.Interfaces
+{
+    public interface IRoleService
+    {
+        Task<List<RoleResponse>> GetRoles();
+        Task<RoleResponse> GetRole(int id);
+        Task AddRole(RoleResponse Role);
+        Task UpdateRole(RoleResponse Role);
+        Task DeleteRole(int id);
+    }
+}
---
+++ BlazorShop.WebClient/Services/RoleService.cs
@@ -0,0 +1,51 @@
+﻿namespace BlazorShop.WebClient.Services
+{
+    public class RoleService : IRoleService
+    {
+        private readonly HttpClient _httpClient;
+
+        public RoleService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task AddRole(RoleResponse role)
+        {
+            await _httpClient.PostAsJsonAsync($"Roles/role", role);
+        }
+
+        public async Task DeleteRole(int id)
+        {
+            await _httpClient.GetAsync($"Roles/role/{id}");
+        }
+
+        public async Task<RoleResponse> GetRole(int id)
+        {
+            var authResult = await _httpClient.GetAsync($"Roles/role/{id}");
+            var authContent = await authResult.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<RoleResponse>(
+                authContent,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+            );
+
+            return result;
+        }
+
+        public async Task<List<RoleResponse>> GetRoles()
+        {
+            var authResult = await _httpClient.GetAsync("Roles/roles");
+            var authContent = await authResult.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<List<RoleResponse>>(
+                authContent,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+            );
+
+            return result;
+        }
+
+        public async Task UpdateRole(RoleResponse role)
+        {
+            await _httpClient.PutAsJsonAsync($"Roles/role", role);
+        }
+    }
+}
