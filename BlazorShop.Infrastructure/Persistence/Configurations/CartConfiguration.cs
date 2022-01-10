700f28fb8a6997c7e4e160e8a4dc2b818ccf7657
JBeni <JBeni@users.noreply.github.com>
2022-01-10 18:17:52 +0200

    create cart entity configuration

---
+++ BlazorShop.Infrastructure/Persistence/Configurations/CartConfiguration.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorShop.Infrastructure.Persistence.Configurations
+{
+    public class CartConfiguration : IEntityTypeConfiguration<Cart>
+    {
+        public void Configure(EntityTypeBuilder<Cart> builder)
+        {
+            builder.ToTable("Carts");
+
+            builder.Property(t => t.Name)
+                .HasMaxLength(200)
+                .IsRequired();
+            builder.Property(p => p.Price)
+                .HasColumnType("decimal(18, 2)");
+        }
+    }
+}
