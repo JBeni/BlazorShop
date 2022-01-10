292234c056ba26b276c854b4f9bb853178b2e1a3
JBeni <JBeni@users.noreply.github.com>
2022-01-10 08:46:27 +0200

    move clothe interface service - blazor web

---
+++ BlazorShop.WebClient/Interfaces/IClotheService.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorShop.WebClient.Interfaces
+{
+    public interface IClotheService
+    {
+        Task<List<ClotheResponse>> GetClothes();
+        Task<ClotheResponse> GetClothe(int id);
+        Task AddClothe(ClotheResponse clothe);
+        Task UpdateClothe(ClotheResponse clothe);
+        Task DeleteClothe(int id);
+    }
+}
