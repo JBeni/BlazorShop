b899fb17a8c57d549b093f211d8fbf56f712e919
JBeni <JBeni@users.noreply.github.com>
2022-01-10 08:46:39 +0200

    create cart interface service - blazor web

---
+++ BlazorShop.WebClient/Interfaces/ICartService.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorShop.WebClient.Interfaces
+{
+    public interface ICartService
+    {
+        Task<List<CartResponse>> GetCarts();
+        Task<int> GetCartCounts();
+        Task<CartResponse> GetCart(int id);
+        Task AddCart(CartResponse Cart);
+        Task UpdateCart(CartResponse Cart);
+        Task DeleteCart(int id);
+    }
+}
