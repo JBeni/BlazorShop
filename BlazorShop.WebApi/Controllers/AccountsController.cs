732db51a1d6212199a983d424e3c463f29151e20
JBeni <JBeni@users.noreply.github.com>
2022-01-11 11:24:36 +0200

    remove the encryption service

--- BlazorShop.WebApi/Controllers/AccountsController.cs
+++ BlazorShop.WebApi/Controllers/AccountsController.cs
@@ -3,18 +3,14 @@
     public class AccountsController : ApiControllerBase
     {
         private readonly IConfiguration _configuration;
-        private readonly IEncryptionService _encryptionService;
         private readonly IEmailService _emailService;
         private readonly ICurrentUserService _currentUserService;
 
-        public AccountsController(
-            IConfiguration configuration,
-            IEncryptionService encryptionService,
-            IEmailService emailService,
-            ICurrentUserService currentUserService)
+        public AccountsController(IConfiguration configuration,
+                                  IEmailService emailService,
+                                  ICurrentUserService currentUserService)
         {
             _configuration = configuration;
-            _encryptionService = encryptionService;
             _emailService = emailService;
             _currentUserService = currentUserService;
         }
@@ -27,14 +23,9 @@
 
         [AllowAnonymous]
         [HttpPost("login")]
-        public async Task<IActionResult> LoginUser([FromForm] LoginCommand loginCommand)
+        public async Task<IActionResult> LoginUser([FromForm] LoginCommand login)
         {
-            //var loginCommand = new LoginCommand()
-            //{
-            //    Email = _encryptionService.DecryptData(login.Email),
-            //    Password = _encryptionService.DecryptData(login.Password)
-            //};
-            var result = await Mediator.Send(loginCommand);
+            var result = await Mediator.Send(login);
 
             return result.Successful == true
                 ? Ok(result)
@@ -43,19 +34,9 @@
 
         [AllowAnonymous]
         [HttpPost("register")]
-        public async Task<IActionResult> RegisterUser([FromForm] RegisterCommand registerCommand)
+        public async Task<IActionResult> RegisterUser([FromForm] RegisterCommand register)
         {
-            //var registerCommand = new RegisterCommand
-            //{
-            //    FirstName = register.FirstName,
-            //    LastName = register.LastName,
-
-            //    Email = _encryptionService.DecryptData(register.Email),
-            //    Password = _encryptionService.DecryptData(register.Password),
-            //    ConfirmPassword = _encryptionService.DecryptData(register.ConfirmPassword)
-            //};
-
-            var result = await Mediator.Send(registerCommand);
+            var result = await Mediator.Send(register);
             return result.Successful == true
                 ? Ok(result)
                 : BadRequest(result.Exception.InnerException.Message ?? result.Exception.Message);
@@ -64,30 +45,16 @@
         [HttpPut("change-password")]
         public async Task<IActionResult> ChangePasswordUser([FromBody] ChangePasswordCommand changePassword)
         {
-            var changeCommand = new ChangePasswordCommand
-            {
-                UserId = int.Parse(_currentUserService.UserId),
-                OldPassword = _encryptionService.DecryptData(changePassword.OldPassword),
-                NewPassword = _encryptionService.DecryptData(changePassword.NewPassword),
-                ConfirmNewPassword = _encryptionService.DecryptData(changePassword.ConfirmNewPassword)
-            };
-
-            var result = await Mediator.Send(changeCommand);
-            return result.Successful == true ? Ok(result) : BadRequest(result.Exception.InnerException.Message ?? result.Exception.Message);
+            var result = await Mediator.Send(changePassword);
+            return result.Successful == true
+                ? Ok(result)
+                : BadRequest(result.Exception.InnerException.Message ?? result.Exception.Message);
         }
 
         [AllowAnonymous]
         [HttpPost("reset-password")]
         public async Task<IActionResult> ResetPasswordUser([FromForm] ResetPasswordCommand resetPassword)
         {
-            var resetCommand = new ResetPasswordCommand
-            {
-                Email = _encryptionService.DecryptData(resetPassword.Email),
-                NewPassword = _encryptionService.DecryptData(resetPassword.NewPassword),
-                NewConfirmPassword = _encryptionService.DecryptData(resetPassword.NewConfirmPassword)
-            };
-
-            var email = _encryptionService.DecryptData(resetPassword.Email);
             var emailSettings = new EmailSettings
             {
                 Host = _configuration["EmailSettings:Host"],
@@ -97,10 +64,12 @@
                 Username = _configuration["EmailSettings:Username"],
                 Password = _configuration["EmailSettings:Password"]
             };
-            await _emailService.SendEmail(email, emailSettings);
+            await _emailService.SendEmail(resetPassword.Email, emailSettings);
 
-            var result = await Mediator.Send(resetCommand);
-            return result.Successful == true ? Ok(result) : BadRequest(result.Exception.InnerException.Message ?? result.Exception.Message);
+            var result = await Mediator.Send(resetPassword);
+            return result.Successful == true
+                ? Ok(result)
+                : BadRequest(result.Exception.InnerException.Message ?? result.Exception.Message);
         }
     }
 }
