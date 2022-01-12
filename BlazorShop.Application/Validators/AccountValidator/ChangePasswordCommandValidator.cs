c25a82ad2661ca7889042a643cb7deea92b0b686
JBeni <JBeni@users.noreply.github.com>
2022-01-12 23:00:59 +0200

    update account validators

--- BlazorShop.Application/Validators/AccountValidator/ChangePasswordCommandValidator.cs
+++ BlazorShop.Application/Validators/AccountValidator/ChangePasswordCommandValidator.cs
@@ -6,8 +6,8 @@
         {
             RuleFor(v => v.UserId).GreaterThan(0);
             RuleFor(v => v.OldPassword).NotEmpty().NotNull().NotEqual(v => v.NewPassword);
-            RuleFor(v => v.NewPassword).NotEmpty().NotNull().Equal(v => v.NewConfirmPassword);
-            RuleFor(v => v.NewConfirmPassword).NotEmpty().NotNull();
+            RuleFor(v => v.NewPassword).NotEmpty().NotNull().Equal(v => v.ConfirmNewPassword);
+            RuleFor(v => v.ConfirmNewPassword).NotEmpty().NotNull();
         }
     }
 }
--- BlazorShop.Application/Validators/AccountValidator/ResetPasswordCommandValidator.cs
+++ BlazorShop.Application/Validators/AccountValidator/ResetPasswordCommandValidator.cs
@@ -5,7 +5,6 @@
         public ResetPasswordCommandValidator()
         {
             RuleFor(v => v.Email).NotEmpty().NotNull();
-            RuleFor(v => v.TokenReset).NotEmpty().NotNull();
             RuleFor(v => v.NewPassword).NotEmpty().NotNull().Equal(v => v.NewConfirmPassword);
             RuleFor(v => v.NewConfirmPassword).NotEmpty().NotNull();
         }
