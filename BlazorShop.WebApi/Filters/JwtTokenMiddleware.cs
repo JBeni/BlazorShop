6468f7d695fdc54aaef701587a646107e4b53f52
JBeni <JBeni@users.noreply.github.com>
2022-01-12 22:38:27 +0200

    add JwtTokenMiddleware

---
+++ BlazorShop.WebApi/Filters/JwtTokenMiddleware.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorShop.WebApi.Filters
+{
+    public class JwtTokenMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public JwtTokenMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var user = context.User.Identity.Name;
+            var role = context.User.IsInRole("Admin");
+
+            await _next(context);
+        }
+    }
+}
