4d20bf8761ad61acb2e7baf332ebf008a865dd7d
JBeni <JBeni@users.noreply.github.com>
2022-01-12 22:58:45 +0200

    update cart commands handlers

---
+++ BlazorShop.Application/Handlers/Commands/CartCommand/CreateCartCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿namespace BlazorShop.Application.Handlers.Commands.CartCommand
+{
+    public class CreateCartCommandHandler : IRequestHandler<CreateCartCommand, RequestResponse>
+    {
+        private readonly IApplicationDbContext _dbContext;
+        private readonly UserManager<AppUser> _userManager;
+
+        public CreateCartCommandHandler(IApplicationDbContext dbContext, UserManager<AppUser> userManager)
+        {
+            _dbContext = dbContext;
+            _userManager = userManager;
+        }
+
+        public async Task<RequestResponse> Handle(CreateCartCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var clothe = _dbContext.Clothes.FirstOrDefault(x => x.Id == request.ClotheId);
+                var user = await _userManager.FindByIdAsync(request.UserId.ToString());
+
+                var entity = new Cart
+                {
+                    Name = request.Name,
+                    Price = request.Price,
+                    Amount = request.Amount,
+                    Clothe = clothe,
+                    User = user
+                };
+
+                _dbContext.Carts.Add(entity);
+                await _dbContext.SaveChangesAsync(cancellationToken);
+                return RequestResponse.Success();
+            }
+            catch (Exception ex)
+            {
+                return RequestResponse.Error(new Exception("There was an error creating the cart", ex));
+            }
+        }
+    }
+}
---
+++ BlazorShop.Application/Handlers/Commands/CartCommand/DeleteAllCartsCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace BlazorShop.Application.Handlers.Commands.CartCommand
+{
+    public class DeleteAllCartsCommandHandler : IRequestHandler<DeleteAllCartsCommand, RequestResponse>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        public DeleteAllCartsCommandHandler(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<RequestResponse> Handle(DeleteAllCartsCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                _dbContext.Carts.RemoveRange(_dbContext.Carts.Where(x => x.User.Id == request.UserId));
+                await _dbContext.SaveChangesAsync(cancellationToken);
+                return RequestResponse.Success();
+            }
+            catch (Exception ex)
+            {
+                return RequestResponse.Error(new Exception("There was an error deleting all the carts", ex));
+            }
+        }
+    }
+}
