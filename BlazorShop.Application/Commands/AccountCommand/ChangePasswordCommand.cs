a67ba046b4303b52ae805b12ea01e8505b65a896
JBeni <JBeni@users.noreply.github.com>
2022-01-12 22:57:15 +0200

    update account commands

--- BlazorShop.Application/Commands/AccountCommand/ChangePasswordCommand.cs
+++ BlazorShop.Application/Commands/AccountCommand/ChangePasswordCommand.cs
@@ -2,11 +2,9 @@
 {
     public class ChangePasswordCommand : IRequest<RequestResponse>
     {
-        public string? Email { get; set; }
-
-        public string? UserId { get; set; }
+        public int UserId { get; set; }
         public string? OldPassword { get; set; }
         public string? NewPassword { get; set; }
-        public string? NewConfirmPassword { get; set; }
+        public string? ConfirmNewPassword { get; set; }
     }
 }
--- BlazorShop.Application/Commands/AccountCommand/ResetPasswordCommand.cs
+++ BlazorShop.Application/Commands/AccountCommand/ResetPasswordCommand.cs
@@ -3,7 +3,6 @@
     public class ResetPasswordCommand : IRequest<RequestResponse>
     {
         public string? Email { get; set; }
-        public string TokenReset => "Bearer Token";
         public string? NewPassword { get; set; }
         public string? NewConfirmPassword { get; set; }
     }
