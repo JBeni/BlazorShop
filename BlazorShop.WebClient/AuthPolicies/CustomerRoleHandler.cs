29abcda29b5c1db9beaf0e8195c5f61efcd244aa
JBeni <JBeni@users.noreply.github.com>
2022-01-15 10:58:10 +0200

    create customer (user + default policy based on role) handler - web client

---
+++ BlazorShop.WebClient/AuthPolicies/CustomerRoleHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace BlazorShop.WebClient.AuthPolicies
+{
+    public class CustomerRoleRequirement : IAuthorizationRequirement
+    {
+        public CustomerRoleRequirement()
+        {
+        }
+    }
+
+    public class CustomerRoleHandler : AuthorizationHandler<CustomerRoleRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, CustomerRoleRequirement requirement)
+        {
+            if (!context.User.Identity.IsAuthenticated) return Task.CompletedTask;
+
+            var defaultRole = context.User.Claims.FirstOrDefault(c => c.Type == "role" && c.Value == "Default");
+            var userRole = context.User.Claims.FirstOrDefault(c => c.Type == "role" && c.Value == "User");
+
+            if (userRole == null && defaultRole == null) return Task.CompletedTask;
+
+            if (userRole.Value.Equals("User") || defaultRole.Value.Equals("Default"))
+            {
+                context.Succeed(requirement);
+                return Task.CompletedTask;
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
