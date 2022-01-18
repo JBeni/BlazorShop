67b014f1a2042b38e96e947fdc7c53630633a1b3
JBeni <JBeni@users.noreply.github.com>
2022-01-18 13:52:42 +0200

    create dependency injection for unit of work

---
+++ BlazorShop.UnitOfWork/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorShop.UnitOfWork
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddUnitOfWorkLayer(this IServiceCollection services)
+        {
+            services.AddTransient<IUnitOfWork, UnitOfWorkPattern>();
+            services.AddTransient<IMusicRepository, MusicRepository>();
+
+            services.AddScoped<MusicService>();
+
+            return services;
+        }
+    }
+}
