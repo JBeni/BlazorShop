22102f53c5e4ee4b2504bf1796cf78c647732d10
JBeni <JBeni@users.noreply.github.com>
2022-01-18 13:52:56 +0200

    create global usings - imports

---
+++ BlazorShop.UnitOfWork/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using Microsoft.Extensions.DependencyInjection;
+global using BlazorShop.Domain.Entities;
+global using BlazorShop.Infrastructure.Persistence;
+global using BlazorShop.UnitOfWork.Interfaces;
+global using AutoMapper;
+global using BlazorShop.Application.Responses;
+global using BlazorShop.UnitOfWork.Repositories;
+global using BlazorShop.UnitOfWork.Services;
