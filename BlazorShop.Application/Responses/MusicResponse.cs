838ee8328357ab45fac76f5f183b5ed390c7265a
JBeni <JBeni@users.noreply.github.com>
2022-01-18 13:51:20 +0200

    create MusicResponse

---
+++ BlazorShop.Application/Responses/MusicResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorShop.Application.Responses
+{
+    public class MusicResponse : IMapFrom<Music>
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Music, MusicResponse>()
+                .ForMember(x => x.Id, opt => opt.MapFrom(s => s.Id))
+                .ForMember(x => x.Title, opt => opt.MapFrom(s => s.Title));
+        }
+
+        public string Error { get; set; }
+    }
+}
