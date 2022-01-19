a8b4e445ba80e8c6af7d8185e121233e716fac4d
JBeni <JBeni@users.noreply.github.com>
2022-01-19 20:44:54 +0200

    create subscription service

---
+++ BlazorShop.WebClient/Interfaces/ISubscriptionService.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorShop.WebClient.Interfaces
+{
+    public interface ISubscriptionService
+    {
+        Task<List<SubscriptionResponse>> GetSubscriptions();
+        Task<SubscriptionResponse> GetSubscription(int id);
+        Task<RequestResponse> AddSubscription(SubscriptionResponse Subscription);
+        Task<RequestResponse> UpdateSubscription(SubscriptionResponse Subscription);
+        Task<RequestResponse> DeleteSubscription(int id);
+    }
+}
---
+++ BlazorShop.WebClient/Services/SubscriptionService.cs
@@ -0,0 +1,89 @@
+﻿namespace BlazorShop.WebClient.Services
+{
+    public class SubscriptionService : ISubscriptionService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IToastService _toastService;
+
+        public SubscriptionService(HttpClient httpClient, IToastService toastService)
+        {
+            _httpClient = httpClient;
+            _toastService = toastService;
+        }
+
+        public async Task<List<SubscriptionResponse>> GetSubscriptions()
+        {
+            var authResult = await _httpClient.GetAsync("Subscriptions/subscriptions");
+            if (authResult.IsSuccessStatusCode == false)
+            {
+                _toastService.ShowError("Something went wrong.");
+                return null;
+            }
+
+            var authContent = await authResult.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<List<SubscriptionResponse>>(
+                authContent,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+            );
+
+            return result;
+        }
+
+        public async Task<SubscriptionResponse> GetSubscription(int id)
+        {
+            var authResult = await _httpClient.GetAsync($"Subscriptions/subscription/{id}");
+            if (authResult.IsSuccessStatusCode == false)
+            {
+                _toastService.ShowError("Something went wrong.");
+                return null;
+            }
+
+            var authContent = await authResult.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<SubscriptionResponse>(
+                authContent,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+            );
+
+            return result;
+        }
+
+        public async Task<RequestResponse> AddSubscription(SubscriptionResponse Subscription)
+        {
+            var response = await _httpClient.PostAsJsonAsync("Subscriptions/subscription", Subscription);
+            if (response.IsSuccessStatusCode == false)
+            {
+                _toastService.ShowError("Something went wrong.");
+                return null;
+            }
+
+            _toastService.ShowSuccess("The Subscription was added.");
+            return RequestResponse.Success();
+        }
+
+        public async Task<RequestResponse> UpdateSubscription(SubscriptionResponse Subscription)
+        {
+            var response = await _httpClient.PutAsJsonAsync("Subscriptions/subscription", Subscription);
+            if (response.IsSuccessStatusCode == false)
+            {
+                _toastService.ShowError("Something went wrong.");
+                return null;
+            }
+
+            _toastService.ShowSuccess("The Subscription was updated.");
+            return RequestResponse.Success();
+        }
+
+        public async Task<RequestResponse> DeleteSubscription(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"Subscriptions/subscription/{id}");
+            if (response.IsSuccessStatusCode == false)
+            {
+                _toastService.ShowError("Something went wrong.");
+                return null;
+            }
+
+            _toastService.ShowSuccess("The Subscription was deleted.");
+            return RequestResponse.Success();
+        }
+    }
+}
