be55a29f16c0ab040e0e81303782cb45b5add03c
JBeni <JBeni@users.noreply.github.com>
2022-01-20 12:42:43 +0200

    add entities to application db context

--- BlazorShop.Application/Common/Interfaces/IApplicationDbContext.cs
+++ BlazorShop.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -2,9 +2,12 @@
 {
     public interface IApplicationDbContext
     {
-        DbSet<Clothe> Clothes { get; set; }
-        DbSet<Order> Orders { get; set; }
         DbSet<Cart> Carts { get; set; }
+        DbSet<Clothe> Clothes { get; set; }
+        DbSet<Music> Musics { get; set; }
+        DbSet<Order> Orders { get; set; }
+        DbSet<Subscriber> Subscribers { get; set; }
+        DbSet<Subscription> Subscriptions { get; set; }
 
         Task<int> SaveChangesAsync(CancellationToken cancellationToken);
     }
