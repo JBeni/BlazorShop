bb9150fe3c4c94ce5cf4643fca7f40a24a7c9a62
JBeni <JBeni@users.noreply.github.com>
2022-01-20 12:45:06 +0200

    create subscriber entity command handlers

---
+++ BlazorShop.Application/Handlers/Commands/SubscriberCommand/DeleteSubscriberCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace BlazorShop.Application.Handlers.Commands.SubscriberCommand
+{
+    public class DeleteSubscriberCommandHandler : IRequestHandler<DeleteSubscriberCommand, RequestResponse>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        public DeleteSubscriberCommandHandler(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<RequestResponse> Handle(DeleteSubscriberCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var entity = _dbContext.Subscribers.FirstOrDefault(x => x.Id == request.Id);
+                if (entity == null) throw new Exception("The entity does not exists");
+
+                _dbContext.Subscribers.Remove(entity);
+                await _dbContext.SaveChangesAsync(cancellationToken);
+                return RequestResponse.Success();
+            }
+            catch (Exception ex)
+            {
+                return RequestResponse.Error(new Exception("There was an error deleting the subscriber", ex));
+            }
+        }
+    }
+}
