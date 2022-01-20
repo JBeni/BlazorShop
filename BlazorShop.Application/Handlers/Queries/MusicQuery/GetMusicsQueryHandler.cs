33b907cba98aceb1d269e4d8e911b297b8d8d321
JBeni <JBeni@users.noreply.github.com>
2022-01-20 12:45:25 +0200

    create music entity query handlers

---
+++ BlazorShop.Application/Handlers/Queries/MusicQuery/GetMusicsQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿namespace BlazorShop.Application.Handlers.Queries.MusicQuery
+{
+    public class GetMusicsQueryHandler : IRequestHandler<GetMusicsQuery, List<MusicResponse>>
+    {
+        private readonly IApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetMusicsQueryHandler(IApplicationDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public Task<List<MusicResponse>> Handle(GetMusicsQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = _dbContext.Musics
+                    .ProjectTo<MusicResponse>(_mapper.ConfigurationProvider)
+                    .ToList();
+                return Task.FromResult(result);
+            }
+            catch (Exception ex)
+            {
+                return Task.FromResult(new List<MusicResponse>
+                {
+                    new MusicResponse
+                    {
+                        Error = "There was an error while getting the musics... " + ex.Message ?? ex.InnerException.Message
+                    }
+                });
+            }
+        }
+    }
+}
