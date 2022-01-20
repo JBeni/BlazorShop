b9f93210d6c50e11eafbaf20c87efd9f4c84d547
JBeni <JBeni@users.noreply.github.com>
2022-01-20 21:47:46 +0200

    create new controllers

---
+++ BlazorShop.WebApi/Controllers/SubscribersController.cs
@@ -0,0 +1,45 @@
+﻿namespace BlazorShop.WebApi.Controllers
+{
+    public class SubscribersController : ApiControllerBase
+    {
+        [Authorize(Roles = "Admin")]
+        [HttpPost("subscriber")]
+        public async Task<IActionResult> CreateSubscriber([FromBody] CreateSubscriberCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return Ok(result);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPut("subscriber")]
+        public async Task<IActionResult> UpdateSubscriber([FromBody] UpdateSubscriberCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return Ok(result);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpDelete("subscriber/{id}")]
+        public async Task<IActionResult> DeleteSubscriber(int id)
+        {
+            var result = await Mediator.Send(new DeleteSubscriberCommand { Id = id });
+            return Ok(result);
+        }
+
+        [Authorize(Roles = "Admin, User, Default")]
+        [HttpGet("subscriber/{id}")]
+        public async Task<IActionResult> GetSubscriber(int id)
+        {
+            var result = await Mediator.Send(new GetSubscriberByIdQuery { Id = id });
+            return Ok(result);
+        }
+
+        [Authorize(Roles = "Admin, User, Default")]
+        [HttpGet("subscribers")]
+        public async Task<IActionResult> GetSubscribers()
+        {
+            var result = await Mediator.Send(new GetSubscribersQuery { });
+            return Ok(result);
+        }
+    }
+}
---
+++ BlazorShop.WebApi/Controllers/SubscriptionsController.cs
@@ -0,0 +1,45 @@
+﻿namespace BlazorShop.WebApi.Controllers
+{
+    public class SubscriptionsController : ApiControllerBase
+    {
+        [Authorize(Roles = "Admin")]
+        [HttpPost("subscription")]
+        public async Task<IActionResult> CreateSubscription([FromBody] CreateSubscriptionCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return Ok(result);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPut("subscription")]
+        public async Task<IActionResult> UpdateSubscription([FromBody] UpdateSubscriptionCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return Ok(result);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpDelete("subscription/{id}")]
+        public async Task<IActionResult> DeleteSubscription(int id)
+        {
+            var result = await Mediator.Send(new DeleteSubscriptionCommand { Id = id });
+            return Ok(result);
+        }
+
+        [Authorize(Roles = "Admin, User, Default")]
+        [HttpGet("subscription/{id}")]
+        public async Task<IActionResult> GetSubscription(int id)
+        {
+            var result = await Mediator.Send(new GetSubscriptionByIdQuery { Id = id });
+            return Ok(result);
+        }
+
+        [Authorize(Roles = "Admin, User, Default")]
+        [HttpGet("subscriptions")]
+        public async Task<IActionResult> GetSubscriptions()
+        {
+            var result = await Mediator.Send(new GetSubscriptionsQuery { });
+            return Ok(result);
+        }
+    }
+}
