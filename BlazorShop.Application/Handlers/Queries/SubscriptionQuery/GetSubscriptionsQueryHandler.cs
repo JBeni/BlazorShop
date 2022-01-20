f87d0e1d551b6c885d99977f432ce72a8e0adc0b
JBeni <JBeni@users.noreply.github.com>
2022-01-20 12:45:49 +0200

    create subscription entity query handlers

---
+++ BlazorShop.Application/Handlers/Queries/SubscriptionQuery/GetSubscriptionsQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿namespace BlazorShop.Application.Handlers.Queries.SubscriptionQuery
+{
+    public class GetSubscriptionsQueryHandler : IRequestHandler<GetSubscriptionsQuery, List<SubscriptionResponse>>
+    {
+        private readonly IApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetSubscriptionsQueryHandler(IApplicationDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public Task<List<SubscriptionResponse>> Handle(GetSubscriptionsQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = _dbContext.Subscriptions
+                    .ProjectTo<SubscriptionResponse>(_mapper.ConfigurationProvider)
+                    .ToList();
+                return Task.FromResult(result);
+            }
+            catch (Exception ex)
+            {
+                return Task.FromResult(new List<SubscriptionResponse>
+                {
+                    new SubscriptionResponse
+                    {
+                        Error = "There was an error while getting the subscriptions... " + ex.Message ?? ex.InnerException.Message
+                    }
+                });
+            }
+        }
+    }
+}
