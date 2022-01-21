867050b3c309e2255e43c78adcfe32c5794dbf3c
Jitca Beniamin <jitca@users.noreply.github.com>
2022-01-21 23:52:36 +0200

    create controllers for invoice, receipt

---
+++ BlazorShop.WebApi/Controllers/InvoicesController.cs
@@ -0,0 +1,45 @@
+﻿namespace BlazorShop.WebApi.Controllers
+{
+    public class InvoicesController : ApiControllerBase
+    {
+        [Authorize(Roles = "Admin, User, Default")]
+        [HttpPost("invoice")]
+        public async Task<IActionResult> CreateInvoice([FromBody] CreateInvoiceCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return Ok(result);
+        }
+
+        [Authorize(Roles = "Admin, User, Default")]
+        [HttpPut("invoice")]
+        public async Task<IActionResult> UpdateInvoice([FromBody] UpdateInvoiceCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return Ok(result);
+        }
+
+        [Authorize(Roles = "Admin, User, Default")]
+        [HttpDelete("invoice/{id}")]
+        public async Task<IActionResult> DeleteInvoice(int id)
+        {
+            var result = await Mediator.Send(new DeleteInvoiceCommand { Id = id });
+            return Ok(result);
+        }
+
+        [Authorize(Roles = "Admin, User, Default")]
+        [HttpGet("invoice/{id}")]
+        public async Task<IActionResult> GetInvoice(int id)
+        {
+            var result = await Mediator.Send(new GetInvoiceByIdQuery { Id = id });
+            return Ok(result);
+        }
+
+        [Authorize(Roles = "Admin, User, Default")]
+        [HttpGet("invoices")]
+        public async Task<IActionResult> GetInvoices()
+        {
+            var result = await Mediator.Send(new GetInvoicesQuery { });
+            return Ok(result);
+        }
+    }
+}
---
+++ BlazorShop.WebApi/Controllers/ReceiptsController.cs
@@ -0,0 +1,45 @@
+﻿namespace BlazorShop.WebApi.Controllers
+{
+    public class ReceiptsController : ApiControllerBase
+    {
+        [Authorize(Roles = "Admin, User, Default")]
+        [HttpPost("receipt")]
+        public async Task<IActionResult> CreateReceipt([FromBody] CreateReceiptCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return Ok(result);
+        }
+
+        [Authorize(Roles = "Admin, User, Default")]
+        [HttpPut("receipt")]
+        public async Task<IActionResult> UpdateReceipt([FromBody] UpdateReceiptCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return Ok(result);
+        }
+
+        [Authorize(Roles = "Admin, User, Default")]
+        [HttpDelete("receipt/{id}")]
+        public async Task<IActionResult> DeleteReceipt(int id)
+        {
+            var result = await Mediator.Send(new DeleteReceiptCommand { Id = id });
+            return Ok(result);
+        }
+
+        [Authorize(Roles = "Admin, User, Default")]
+        [HttpGet("receipt/{id}")]
+        public async Task<IActionResult> GetReceipt(int id)
+        {
+            var result = await Mediator.Send(new GetReceiptByIdQuery { Id = id });
+            return Ok(result);
+        }
+
+        [Authorize(Roles = "Admin, User, Default")]
+        [HttpGet("receipts")]
+        public async Task<IActionResult> GetReceipts()
+        {
+            var result = await Mediator.Send(new GetReceiptsQuery { });
+            return Ok(result);
+        }
+    }
+}
