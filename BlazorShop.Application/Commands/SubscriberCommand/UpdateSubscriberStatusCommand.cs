76028aff82b89388dab42d0cf47d9cfd2f2fe42d
Jitca Beniamin <JBeni@users.noreply.github.com>
2022-01-22 23:10:33 +0200

    remove files

--- BlazorShop.Application/Commands/SubscriberCommand/UpdateSubscriberStatusCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BlazorShop.Application.Commands.SubscriberCommand
-{
-    public class UpdateSubscriberStatusCommand : IRequest<RequestResponse>
-    {
-		public int Id { get; set; }
-		public SubscriptionStatus Status { get; set; }
-	}
-}
