0c3eb4bb1516d9400886cd553d62a93dcf7ccf7f
Jitca Beniamin <JBeni@users.noreply.github.com>
2022-01-25 08:42:23 +0200

    create error view model for web client

---
+++ BlazorShop.WebClient/Error/ErrorView.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorShop.WebClient.Error
+{
+    public class ErrorView
+    {
+        public int Status { get; set; }
+        public string Title { get; set; }
+        public string Detail { get; set; }
+        public string Type { get; set; }
+    }
+}
