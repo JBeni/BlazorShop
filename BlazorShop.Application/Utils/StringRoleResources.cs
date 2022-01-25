431a7e1e19d3b9c646dc1e0512685ca176cedf39
Jitca Beniamin <JBeni@users.noreply.github.com>
2022-01-25 08:36:06 +0200

    create string resources for identity roles

---
+++ BlazorShop.Application/Utils/StringRoleResources.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorShop.Application.Utils
+{
+    public static class StringRoleResources
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+        public const string Default = "Default";
+
+        public const string Customer = "Customer";
+
+        public const string AdminNormalized = "ADMIN";
+        public const string UserNormalized = "USER";
+        public const string DefaultNormalized = "DEFAULT";
+
+        public const string UserIdClaim = "UserId";
+        public const string NameClaim = "unique_name";
+        public const string EmailClaim = "email";
+        public const string RoleClaim = "role";
+    }
+}
