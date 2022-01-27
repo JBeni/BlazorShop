d69d05108059170d8e4eddaa2ae758b14b304ea7
Jitca Beniamin <JBeni@users.noreply.github.com>
2022-01-27 09:43:13 +0200

    updates responses

--- BlazorShop.Application/Responses/RequestResponse.cs
+++ BlazorShop.Application/Responses/RequestResponse.cs
@@ -1,26 +1,18 @@
 ﻿namespace BlazorShop.Application.Responses
 {
-    public class RequestResponse<T> where T : class
+    public class RequestResponse
     {
         public bool Successful { get; set; }
-        public List<string>? Errors { get; set; }
+        public Exception? Exception { get; set; }
 
-        public T Item { get; set; }
-        public List<T> Items { get; set; }
+        public static RequestResponse Success()
+        {
+            return new RequestResponse { Successful = true, Exception = null };
+        }
 
-
-        //public Exception? Exception { get; set; }
-
-        //public static RequestResponse<T> Success()
-        //{
-        //    return new RequestResponse<T> { Successful = true, Exception = null };
-        //}
-
-        //public static RequestResponse<T> Error(Exception exception)
-        //{
-        //    Errors.Add(exception.Message);
-        //    Errors.Add(exception.InnerException.Message);
-        //    return new RequestResponse<T> { Successful = false, Exception = exception };
-        //}
+        public static RequestResponse Error(Exception exception)
+        {
+            return new RequestResponse { Successful = false, Exception = exception };
+        }
     }
 }
---
+++ BlazorShop.Application/Responses/Result.cs
@@ -0,0 +1,26 @@
+﻿namespace BlazorShop.Application.Responses
+{
+    public class Result<T> where T : class
+    {
+        public bool Successful { get; set; }
+        public List<string>? Errors { get; set; }
+
+        public T Item { get; set; }
+        public List<T> Items { get; set; }
+
+
+        //public Exception? Exception { get; set; }
+
+        //public static RequestResponse<T> Success()
+        //{
+        //    return new RequestResponse<T> { Successful = true, Exception = null };
+        //}
+
+        //public static RequestResponse<T> Error(Exception exception)
+        //{
+        //    Errors.Add(exception.Message);
+        //    Errors.Add(exception.InnerException.Message);
+        //    return new RequestResponse<T> { Successful = false, Exception = exception };
+        //}
+    }
+}
