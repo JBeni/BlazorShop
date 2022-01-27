9f04d7f4a3e49126786d9145b779b46e8c934eb4
Jitca Beniamin <JBeni@users.noreply.github.com>
2022-01-28 00:05:50 +0200

    update error view model on client interface

--- BlazorShop.WebClient/Error/ErrorView.cs
+++ BlazorShop.WebClient/Error/ErrorView.cs
@@ -3,8 +3,11 @@
     public class ErrorView
     {
         public int Status { get; set; }
-        public string Title { get; set; }
-        public string Detail { get; set; }
-        public string Type { get; set; }
+        public string? Title { get; set; }
+        public string? Detail { get; set; }
+        public string? Type { get; set; }
+
+        public bool Successful { get; set; }
+        public string? Error { get; set; }
     }
 }
