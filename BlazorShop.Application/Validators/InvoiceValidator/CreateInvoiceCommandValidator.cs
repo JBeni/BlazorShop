9568bc31a178b7db878d54b13392e0bf7b0daf65
Jitca Beniamin <JBeni@users.noreply.github.com>
2022-01-29 08:41:00 +0200

    uadd error messages to invoice validators

--- BlazorShop.Application/Validators/InvoiceValidator/CreateInvoiceCommandValidator.cs
+++ BlazorShop.Application/Validators/InvoiceValidator/CreateInvoiceCommandValidator.cs
@@ -4,11 +4,24 @@
     {
         public CreateInvoiceCommandValidator()
         {
-            RuleFor(v => v.UserEmail).NotEmpty().NotNull();
-            RuleFor(v => v.Name).NotEmpty().NotNull();
-            RuleFor(v => v.AmountSubTotal).GreaterThan(0);
-            RuleFor(v => v.AmountTotal).GreaterThan(0);
-            RuleFor(v => v.Quantity).GreaterThan(0);
+            RuleFor(v => v.UserEmail)
+                .MaximumLength(100).WithMessage("Maximum length exceeded")
+                .NotEmpty().WithMessage("UserEmail must not be empty")
+                .NotNull().WithMessage("UserEmail must not be null");
+
+            RuleFor(v => v.Name)
+                .MaximumLength(200).WithMessage("Maximum length exceeded")
+                .NotEmpty().WithMessage("Name must not be empty")
+                .NotNull().WithMessage("Name must not be null");
+
+            RuleFor(v => v.AmountSubTotal)
+                .GreaterThan(0).WithMessage("AmountSubTotal must be greater than 0");
+
+            RuleFor(v => v.AmountTotal)
+                .GreaterThan(0).WithMessage("AmountTotal must be greater than 0");
+
+            RuleFor(v => v.Quantity)
+                .GreaterThan(0).WithMessage("Quantity must be greater than 0");
         }
     }
 }
--- BlazorShop.Application/Validators/InvoiceValidator/UpdateInvoiceCommandValidator.cs
+++ BlazorShop.Application/Validators/InvoiceValidator/UpdateInvoiceCommandValidator.cs
@@ -4,12 +4,27 @@
     {
         public UpdateInvoiceCommandValidator()
         {
-            RuleFor(v => v.Id).GreaterThan(0);
-            RuleFor(v => v.UserEmail).NotEmpty().NotNull();
-            RuleFor(v => v.Name).NotEmpty().NotNull();
-            RuleFor(v => v.AmountSubTotal).GreaterThan(0);
-            RuleFor(v => v.AmountTotal).GreaterThan(0);
-            RuleFor(v => v.Quantity).GreaterThan(0);
+            RuleFor(v => v.Id)
+                .GreaterThan(0).WithMessage("Id must be greater than 0");
+
+            RuleFor(v => v.UserEmail)
+                .MaximumLength(100).WithMessage("Maximum length exceeded")
+                .NotEmpty().WithMessage("UserEmail must not be empty")
+                .NotNull().WithMessage("UserEmail must not be null");
+
+            RuleFor(v => v.Name)
+                .MaximumLength(200).WithMessage("Maximum length exceeded")
+                .NotEmpty().WithMessage("Name must not be empty")
+                .NotNull().WithMessage("Name must not be null");
+
+            RuleFor(v => v.AmountSubTotal)
+                .GreaterThan(0).WithMessage("AmountSubTotal must be greater than 0");
+
+            RuleFor(v => v.AmountTotal)
+                .GreaterThan(0).WithMessage("AmountTotal must be greater than 0");
+
+            RuleFor(v => v.Quantity)
+                .GreaterThan(0).WithMessage("Quantity must be greater than 0");
         }
     }
 }
