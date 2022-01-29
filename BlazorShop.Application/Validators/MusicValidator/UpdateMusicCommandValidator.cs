b06ee77e68f8e7e6ff5e6c5dc74dfc9db9548bcf
Jitca Beniamin <JBeni@users.noreply.github.com>
2022-01-29 08:41:07 +0200

    uadd error messages to music validators

--- BlazorShop.Application/Validators/MusicValidator/UpdateMusicCommandValidator.cs
+++ BlazorShop.Application/Validators/MusicValidator/UpdateMusicCommandValidator.cs
@@ -4,14 +4,40 @@
     {
         public UpdateMusicCommandValidator()
         {
-            RuleFor(x => x.Id).GreaterThan(0);
-            RuleFor(x => x.Title).NotEmpty().NotNull();
-            RuleFor(x => x.Description).NotEmpty().NotNull();
-            RuleFor(x => x.Author).NotEmpty().NotNull();
-            RuleFor(x => x.DateRelease).NotEmpty().NotNull();
-            RuleFor(x => x.ImageName).NotEmpty().NotNull();
-            RuleFor(x => x.ImagePath).NotEmpty().NotNull();
-            RuleFor(x => x.AccessLevel).GreaterThan(0);
+            RuleFor(x => x.Id)
+                .GreaterThan(0).WithMessage("Id must be greater than 0");
+
+            RuleFor(x => x.Title)
+                .MaximumLength(200).WithMessage("Maximum length exceeded")
+                .NotEmpty().WithMessage("Title must not be empty")
+                .NotNull().WithMessage("Title must not be null");
+
+            RuleFor(x => x.Description)
+                .MaximumLength(1000).WithMessage("Maximum length exceeded")
+                .NotEmpty().WithMessage("Description must not be empty")
+                .NotNull().WithMessage("Description must not be null");
+
+            RuleFor(x => x.Author)
+                .NotEmpty().WithMessage("Author must not be empty")
+                .NotNull().WithMessage("Author must not be null");
+
+            RuleFor(x => x.DateRelease)
+                .GreaterThan(new DateTime(1950, 1, 1)).WithMessage("Date must be greater than 1 January 1950")
+                .NotEmpty().WithMessage("DateRelease must not be empty")
+                .NotNull().WithMessage("DateRelease must not be null");
+
+            RuleFor(x => x.ImageName)
+                .MaximumLength(200).WithMessage("Maximum length exceeded")
+                .NotEmpty().WithMessage("ImageName must not be empty")
+                .NotNull().WithMessage("ImageName must not be null");
+
+            RuleFor(x => x.ImagePath)
+                .MaximumLength(200).WithMessage("Maximum length exceeded")
+                .NotEmpty().WithMessage("ImagePath must not be empty")
+                .NotNull().WithMessage("ImagePath must not be null");
+
+            RuleFor(x => x.AccessLevel)
+                .GreaterThan(0).WithMessage("AccessLevel must be greater than 0");
         }
     }
 }
