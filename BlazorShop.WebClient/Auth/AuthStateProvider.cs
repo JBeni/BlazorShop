8de87f6f826b9e94e057fffdf53d918ca82497be
Jitca Beniamin <JBeni@users.noreply.github.com>
2022-06-11 19:21:22 +0100

    logout the user when token is expired - client side settings

--- BlazorShop.WebClient/Auth/AuthStateProvider.cs
+++ BlazorShop.WebClient/Auth/AuthStateProvider.cs
@@ -1,27 +1,37 @@
-﻿namespace BlazorShop.WebClient.Auth
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace BlazorShop.WebClient.Auth
 {
     public class AuthStateProvider : AuthenticationStateProvider
     {
         private readonly HttpClient _httpClient;
         private readonly ILocalStorageService _localStorage;
         private readonly AuthenticationState _anonymous;
+        private readonly IConfiguration _configuration;
+        private readonly NavigationManager _navMagager;
 
-        public AuthStateProvider(HttpClient httpClient, ILocalStorageService localStorage)
+        public AuthStateProvider(HttpClient httpClient, ILocalStorageService localStorage, IConfiguration configuration, NavigationManager navMagager)
         {
             _httpClient = httpClient;
             _localStorage = localStorage;
             _anonymous = new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+            _configuration = configuration;
+            _navMagager = navMagager;
         }
 
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
             var token = await _localStorage.GetItemAsync<string>("authToken");
 
-            if (string.IsNullOrWhiteSpace(token))
+            if (string.IsNullOrWhiteSpace(token) || !this.IsCurrentTokenValid(token))
             {
+                this.NotifyUserLogout();
+                _navMagager.NavigateTo("/login");
                 return _anonymous;
             }
-            
+
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.ToString());
             return new AuthenticationState(
                     new ClaimsPrincipal(new ClaimsIdentity(JwtTokenParser.ParseClaimsFromJwt(token), "jwtAuthType"))
@@ -39,6 +49,32 @@
         {
             var authState = Task.FromResult(_anonymous);
             NotifyAuthenticationStateChanged(authState);
+        }
+
+        private bool IsCurrentTokenValid(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            try
+            {
+                tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ClockSkew = TimeSpan.FromSeconds(1),
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtToken:SecretKey"])),
+                    RequireSignedTokens = true,
+                    RequireExpirationTime = true,
+                    ValidateLifetime = true,
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidAudience = _configuration["JwtToken:Audience"],
+                    ValidIssuer = _configuration["JwtToken:Issuer"]
+                }, out SecurityToken validatedToken);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
