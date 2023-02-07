c75e83a925b1b287e0875ec9a417d8b0758f6e41
Jitca Beniamin <JBeni@users.noreply.github.com>
2023-02-07 21:56:46 +0000

    add more doc

--- BlazorShop.WebApi.Tests/Application/Handlers/Commands/CartHandler/CreateCartCommandHandlerTests.cs
+++ BlazorShop.WebApi.Tests/Application/Handlers/Commands/CartHandler/CreateCartCommandHandlerTests.cs
@@ -4,12 +4,7 @@
 
 using BlazorShop.Application.Handlers.Commands.CartHandler;
 using BlazorShop.Infrastructure.Persistence;
-using Microsoft.AspNetCore.DataProtection;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Moq;
-using Newtonsoft.Json;
-using Stripe;
-using System.Xml.Linq;
 using Xunit;
 
 namespace BlazorShop.WebApi.Tests.Application.Handlers.Commands.CartHandler
@@ -33,7 +28,7 @@ namespace BlazorShop.WebApi.Tests.Application.Handlers.Commands.CartHandler
             this.UserManager = new UserManager<User>(userStore, null, null, null, null, null, null, null, null);
 
             // Create a UserManager<User> instance
-            //this.UserManager = new UserManager<User>(mockUserStore.Object, options, new PasswordHasher<User>(),
+            // this.UserManager = new UserManager<User>(mockUserStore.Object, options, new PasswordHasher<User>(),
             //    new IUserValidator<User>[] { new UserValidator<User>() },
             //    new IPasswordValidator<User>[] { new PasswordValidator<User>() },
             //    new UpperInvariantLookupNormalizer(),
@@ -63,32 +58,9 @@ namespace BlazorShop.WebApi.Tests.Application.Handlers.Commands.CartHandler
         private UserManager<User> UserManager { get; }
 
         /// <summary>
-        /// Gets the instance of the <see cref="IAccountService"/> to use.
-        /// </summary>
-        private IAccountService AccountService { get; } = Mock.Of<IAccountService>();
-
-        /// <summary>
         /// Gets the <see cref="ILogger{CreateCartCommandHandler}"/> under test.
         /// </summary>
         private ILogger<CreateCartCommandHandler> Logger { get; } = Mock.Of<ILogger<CreateCartCommandHandler>>();
-
-        /// <summary>
-        /// Ensure context garbage collector.
-        /// </summary>
-        public void Dispose()
-        {
-            this.Dispose(disposing: true);
-            GC.SuppressFinalize(this);
-        }
-
-        /// <summary>
-        /// Ensure the context is reset between tests.
-        /// </summary>
-        /// <param name="disposing">A value indicating whether the class is disposing.</param>
-        protected virtual void Dispose(bool disposing)
-        {
-            // this.ApplicationDbContext.Database.EnsureDeleted();
-        }
 
         /// <summary>
         /// A test for <see cref="CreateCartCommandHandler.Handle(CreateCartCommand, CancellationToken)"/> method.
@@ -146,19 +118,10 @@ namespace BlazorShop.WebApi.Tests.Application.Handlers.Commands.CartHandler
             //Assert.Equal(result.Successful, response.Successful);
             //Assert.Equal(result.Error, response.Error);
             //Assert.Equal(result.EntityId, response.EntityId);
-
-            //Assert.Equal(appointment.Object.Title, result.Title);
-            //Assert.Equal(appointment.Object.StartDateTime, result.StartDateTime);
-            //Assert.Equal(appointment.Object.EndDateTime, result.EndDateTime);
-            //Assert.Equal(appointment.Object.IsOpenAppointment, result.IsOpenAppointment);
-            //Assert.Equal(appointment.Object.LocalAuthority, result.LocalAuthority);
-            //Assert.Equal(appointment.Object.SearcherActiveDirectoryId, result.SearcherActiveDirectoryId);
-            //Assert.Equal(appointment.Object.TypeName, result.TypeName);
-            //Assert.Equal(appointment.Object.Status, result.Status);
         }
 
         /// <summary>
-        /// A test for <see cref="ResetPasswordCommandHandler.Handle(CreateCartCommand, CancellationToken)"/> method.
+        /// A test for <see cref="CreateCartCommandHandler.Handle(CreateCartCommand, CancellationToken)"/> method.
         /// </summary>
         /// <returns>A <see cref="Task{RequestResponse}"/>.</returns>
         [Fact]
@@ -180,114 +143,21 @@ namespace BlazorShop.WebApi.Tests.Application.Handlers.Commands.CartHandler
             Assert.Equal(result.EntityId, response.EntityId);
         }
 
-
-        /*
-
-                private void SetupIndexDbSets()
-                {
-                    this.MockCoreContext.SetupDbSet(
-                        x => x.EventCategoryIndex,
-                        (this.MockCoreContext.Object.SearchEvent ?? Enumerable.Empty<SearchEvent>())
-                            .ToList()
-                            .SelectMany(e => e.Type.EventCategories.Select(c => new
-                            {
-                                EventId = e.Id,
-                                SearchId = e.SearchId,
-                                CreatedAt = e.CreatedAt,
-                                EventType = e.Type.Name,
-                                Category = c.EventCategory.Name,
-                            }))
-                            .GroupBy(e => new { e.SearchId, e.Category })
-                            .Select(g => g.OrderByDescending(x => x.CreatedAt).First())
-                            .Select(x => new EventCategoryIndex(x.SearchId, x.EventId, x.Category, x.EventType))
-                            .ToList());
-
-                    this.MockCoreContext.SetupDbSet(
-                        x => x.AppointmentIndex,
-                        (this.MockCoreContext.Object.Appointments ?? Enumerable.Empty<Appointment>()).ToList()
-                            .Select(a => new AppointmentIndex(a.History.OrderByDescending(h => h.CreatedAt).First()))
-                            .ToList());
-                }
-
-
-                /// <summary>
-        /// Ensures that the <see cref="IAppointmentsRepository.CreateAppointment(IAppointment)"/> method
-        /// saves the provided <see cref="Appointment"/> entity to the database and returns a business object
-        /// representation of the appointment.
+        /// <summary>
+        /// Ensure garbage collector for db context.
         /// </summary>
-        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
-        [Fact]
-        public async Task CreateAppointment()
+        public void Dispose()
         {
-            var appointment = new Mock<IAppointment>();
-            appointment.SetupGet(x => x.Title).Returns("Title");
-            appointment.SetupGet(x => x.StartDateTime).Returns(DateTimeOffset.UtcNow);
-            appointment.SetupGet(x => x.EndDateTime).Returns(DateTimeOffset.UtcNow);
-            appointment.SetupGet(x => x.IsOpenAppointment).Returns(false);
-            appointment.SetupGet(x => x.LocalAuthority).Returns("Local Authority");
-            appointment.SetupGet(x => x.SearcherActiveDirectoryId).Returns("Searcher ID");
-            appointment.SetupGet(x => x.TypeName).Returns("LA Appointment");
-            appointment.SetupGet(x => x.Status).Returns(Core.Models.Scheduling.AppointmentStatuses.Initial);
-
-            var appointmentTypeEntity = new Entities.Scheduling.AppointmentType(
-                "LA Appointment",
-                "LA Appointment description",
-                DateTimeOffset.UtcNow,
-                "System");
-            this.CoreContext.AppointmentTypes.Add(appointmentTypeEntity);
-
-            this.CoreContext.Authorities.Add(new Authority
-            {
-                Name = appointment.Object.LocalAuthority,
-                AuthorityTypeName = "LocalAuthority",
-                Code = "TEST",
-            });
-
-            this.CoreContext.SaveChanges();
-
-            EventHandler<EntityTrackedEventArgs> handler = (source, e) =>
-            {
-                if (e.Entry.Entity is AppointmentHistory)
-                {
-                    var entity = e.Entry.Entity as AppointmentHistory;
-                    this.CoreContext.AppointmentIndex.Add(new AppointmentIndex(
-                        entity.AppointmentId,
-                        entity.Id,
-                        entity.StartDateTime,
-                        entity.EndDateTime,
-                        entity.Authority.Name,
-                        entity.Department,
-                        entity.SearcherActiveDirectoryId,
-                        entity.Status));
-                    this.CoreContext.SaveChanges();
-                }
-            };
-
-            this.CoreContext.ChangeTracker.Tracked += handler;
-
-            var result = await this.AlternateAppointmentsRepository.CreateAppointment(appointment.Object);
-
-            this.CoreContext.ChangeTracker.Tracked -= handler;
-
-            Assert.Equal(appointment.Object.Title, result.Title);
-            Assert.Equal(appointment.Object.StartDateTime, result.StartDateTime);
-            Assert.Equal(appointment.Object.EndDateTime, result.EndDateTime);
-            Assert.Equal(appointment.Object.IsOpenAppointment, result.IsOpenAppointment);
-            Assert.Equal(appointment.Object.LocalAuthority, result.LocalAuthority);
-            Assert.Equal(appointment.Object.SearcherActiveDirectoryId, result.SearcherActiveDirectoryId);
-            Assert.Equal(appointment.Object.TypeName, result.TypeName);
-            Assert.Equal(appointment.Object.Status, result.Status);
+            this.Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
 
-
-
-        */
-
-
-
-
-
-
-
+        /// <summary>
+        /// Ensure the context is reset.
+        /// </summary>
+        /// <param name="disposing">A value indicating whether the class is disposing.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+        }
     }
 }
--- BlazorShop.WebApi.Tests/Application/Handlers/Commands/ClotheHandler/CreateClotheCommandHandlerTests.cs
+++ BlazorShop.WebApi.Tests/Application/Handlers/Commands/ClotheHandler/CreateClotheCommandHandlerTests.cs
@@ -2,34 +2,127 @@
 // Copyright (c) Beniamin Jitca. All rights reserved.
 // </copyright>
 
+using BlazorShop.Application.Handlers.Commands.ClotheHandler;
+using BlazorShop.Infrastructure.Persistence;
+using Moq;
+using Xunit;
+
 namespace BlazorShop.WebApi.Tests.Application.Handlers.Commands.ClotheHandler
 {
     /// <summary>
     /// Tests for <see cref="CreateClotheCommandHandler"/>.
     /// </summary>
-    public class CreateClotheCommandHandlerTests
+    public class CreateClotheCommandHandlerTests : IDisposable
     {
-        private IApplicationDbContext DbContext { get; }
-        private ILogger<CreateClotheCommandHandlerTests> Logger { get; }
-
         /// <summary>
         /// Initializes a new instance of the <see cref="CreateClotheCommandHandlerTests"/> class.
         /// </summary>
-        public CreateClotheCommandHandlerTests(IApplicationDbContext dbContext, ILogger<CreateClotheCommandHandlerTests> logger)
+        public CreateClotheCommandHandlerTests()
         {
-            this.DbContext = dbContext;
-            this.Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this.ApplicationDbContext = new ApplicationDbContext(
+                new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseInMemoryDatabase(databaseName: $"ApplicationDbContext-Core")
+                    .Options);
+
+            this.SUT = new CreateClotheCommandHandler(
+                this.ApplicationDbContext,
+                this.Logger);
         }
 
         /// <summary>
-        /// An implementation of the handler for <see cref="DeleteSubscriberCommand"/>.
+        /// Gets the <see cref="CreateClotheCommandHandler"/> instance to use.
         /// </summary>
-        /// <param name="request">The request object to handle.</param>
-        /// <param name="cancellationToken">The cancellation token.</param>
+        private CreateClotheCommandHandler SUT { get; }
+
+        /// <summary>
+        /// Gets the <see cref="IApplicationDbContext"/> under test.
+        /// </summary>
+        private IApplicationDbContext ApplicationDbContext { get; }
+
+        /// <summary>
+        /// Gets the <see cref="ILogger{CreateClotheCommandHandler}"/> under test.
+        /// </summary>
+        private ILogger<CreateClotheCommandHandler> Logger { get; } = Mock.Of<ILogger<CreateClotheCommandHandler>>();
+
+        /// <summary>
+        /// A test for <see cref="CreateClotheCommandHandler.Handle(CreateClotheCommand, CancellationToken)"/> method.
+        /// </summary>
         /// <returns>A <see cref="Task{RequestResponse}"/>.</returns>
-        public Task Handle(CreateClotheCommand request, CancellationToken cancellationToken)
+        [Fact]
+        public async Task Handle()
         {
-            throw new Exception();
+            var createClotheCommand = Mock.Of<CreateClotheCommand>(q =>
+                q.Description == string.Empty &&
+                q.ImageName == string.Empty &&
+                q.ImagePath == string.Empty &&
+                q.Name == string.Empty &&
+                q.Price == new decimal(new Random().NextDouble()) &&
+                q.IsActive == true);
+
+            var clotheEntity = Mock.Of<Clothe>(q =>
+                q.Description == createClotheCommand.Description &&
+                q.ImageName == createClotheCommand.ImageName &&
+                q.ImagePath == createClotheCommand.ImagePath &&
+                q.Name == createClotheCommand.Name &&
+                q.Price == createClotheCommand.Price &&
+                q.IsActive == createClotheCommand.IsActive);
+
+            var response = Mock.Of<RequestResponse>(x =>
+                x.Successful == true &&
+                x.Error == null &&
+                x.EntityId == 1);
+
+            var result = await this.SUT.Handle(createClotheCommand, default);
+
+            var clotheEntityDb = await this.ApplicationDbContext.Clothes
+                .FirstOrDefaultAsync(x => x.Id == result.EntityId);
+
+            Assert.Equal(result.Successful, response.Successful);
+            Assert.Equal(result.Error, response.Error);
+            Assert.Equal(result.EntityId, response.EntityId);
+
+            Assert.Equal(clotheEntity.Description, clotheEntityDb.Description);
+            Assert.Equal(clotheEntity.ImagePath, clotheEntityDb.ImagePath);
+            Assert.Equal(clotheEntity.ImageName, clotheEntityDb.ImageName);
+            Assert.Equal(clotheEntity.Name, clotheEntityDb.Name);
+            Assert.Equal(clotheEntity.Price, clotheEntityDb.Price);
+            Assert.Equal(clotheEntity.IsActive, clotheEntityDb.IsActive);
+        }
+
+        /// <summary>
+        /// A test for <see cref="ResetPasswordCommandHandler.Handle(CreateCartCommand, CancellationToken)"/> method.
+        /// </summary>
+        /// <returns>A <see cref="Task{RequestResponse}"/>.</returns>
+        [Fact]
+        public async Task Handle_ThrowException()
+        {
+            var response = Mock.Of<RequestResponse>(x =>
+                x.Successful == false &&
+                x.Error == ErrorsManager.CreateCartCommand &&
+                x.EntityId == 0);
+
+            var result = await this.SUT.Handle(It.IsAny<CreateClotheCommand>(), default);
+
+            Assert.Equal(result.Successful, response.Successful);
+            Assert.Contains(response.Error, result.Error);
+            Assert.Equal(result.EntityId, response.EntityId);
+        }
+
+        /// <summary>
+        /// Ensure garbage collector for db context.
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Ensure the context is reset.
+        /// </summary>
+        /// <param name="disposing">A value indicating whether the class is disposing.</param>
+        protected virtual void Dispose(bool disposing)
+        {
         }
     }
 }
