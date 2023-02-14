d98c4cb674342803795673ff2842c3f72c1adee0
Jitca Beniamin <JBeni@users.noreply.github.com>
2023-02-14 10:04:35 +0000

    add test for email service

--- BlazorShop.WebApi.Tests/Infrastructure/EmailServiceTests.cs
+++ BlazorShop.WebApi.Tests/Infrastructure/EmailServiceTests.cs
@@ -2,6 +2,10 @@
 // Copyright (c) Beniamin Jitca. All rights reserved.
 // </copyright>
 
+using BlazorShop.Infrastructure.Services;
+using Moq;
+using Xunit;
+
 namespace BlazorShop.WebApi.Tests.Infrastructure
 {
     /// <summary>
@@ -14,6 +18,43 @@ namespace BlazorShop.WebApi.Tests.Infrastructure
         /// </summary>
         public EmailServiceTests()
         {
+        }
+
+        /// <summary>
+        /// Gets the instance of <see cref="IEmailService"/> to use.
+        /// </summary>
+        private IEmailService EmailService { get; } = Mock.Of<IEmailService>();
+
+        /// <summary>
+        /// A test for <see cref="EmailService.SendEmail(string?, EmailSettings)"/> method.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> operation.</returns>
+        [Fact]
+        public async Task SendEmail_Success()
+        {
+            var mailSettings = Mock.Of<EmailSettings>(x =>
+                x.Host == "smtp.gmail.com" &&
+                x.Port == 587 &&
+                x.Username == "example@gmail.com" &&
+                x.Password == "password" &&
+                x.Subject == "Test Email" &&
+                x.Message == "This is a test email.");
+
+            await this.EmailService.SendEmail("recipient@example.com", mailSettings);
+        }
+
+        /// <summary>
+        /// A test for <see cref="EmailService.SendEmail(string?, EmailSettings)"/> method.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> operation.</returns>
+        [Fact]
+        public async Task SendEmail_NullRecipient_ThrowsArgumentNullException()
+        {
+            var mailSettings = Mock.Of<EmailSettings>();
+
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            Assert.ThrowsAsync<ArgumentException>(async () => await this.EmailService.SendEmail(null, mailSettings));
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
         }
     }
 }
