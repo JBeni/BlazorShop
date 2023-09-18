5ca6ff014278e83a367cc2dbfb0bf66fff5e2856
Jitca Beniamin <JBeni@users.noreply.github.com>
2023-09-18 22:23:23 +0100

    adding migrations and new table config

---
+++ BlazorShop.Infrastructure/Services/CustomClaimsService.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="CustomClaimsService.cs" company="Beniamin Jitca">
+// Copyright (c) Beniamin Jitca. All rights reserved.
+// </copyright>
+
+namespace BlazorShop.Infrastructure.Services
+{
+    /// <summary>
+    /// An implementation of <see cref="ICustomClaimsService"/>.
+    /// </summary>
+    public class CustomClaimsService : ICustomClaimsService
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomClaimsService"/> class.
+        /// </summary>
+        /// <param name="userManager">The instance of <see cref="UserManager{User}"/> to use.</param>
+        public CustomClaimsService(UserManager<User> userManager)
+        {
+            this.UserManager = userManager;
+        }
+
+        /// <summary>
+        /// Gets the instance of the <see cref="UserManager{User}"/> to use.
+        /// </summary>
+        private UserManager<User> UserManager { get; }
+
+        /// <inheritdoc/>
+        public Task AddUserClaim()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public Task DeleteUserClaim()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public Task UpdateUserClaim()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BlazorShop.Infrastructure/Services/RoleService.cs
+++ BlazorShop.Infrastructure/Services/RoleService.cs
@@ -149,7 +149,7 @@ namespace BlazorShop.Infrastructure.Services
         }
 
         /// <inheritdoc/>
-        public async Task<RequestResponse> CreateRoleAsync(CreateRoleCommand command)
+        public async Task<RequestResponse> CreateRoleAsync(CreateClaimCommand command)
         {
             var role = await this.RoleManager.FindByNameAsync(command.Name);
             if (role != null)
@@ -168,7 +168,7 @@ namespace BlazorShop.Infrastructure.Services
         }
 
         /// <inheritdoc/>
-        public async Task<RequestResponse> UpdateRoleAsync(UpdateRoleCommand command)
+        public async Task<RequestResponse> UpdateRoleAsync(UpdateClaimCommand command)
         {
             var existsRole = await this.RoleManager.FindByNameAsync(command.Name);
             if (existsRole != null)
