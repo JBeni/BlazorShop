94c35298a6917954aac612b3bbab12eea2fa70df
Jitca Beniamin <JBeni@users.noreply.github.com>
2023-09-18 22:24:14 +0100

    commands

---
+++ BlazorShop.Application/Commands/ClaimCommand/UpdateClaimCommand.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="UpdateClaimCommand.cs" company="Beniamin Jitca">
+// Copyright (c) Beniamin Jitca. All rights reserved.
+// </copyright>
+
+namespace BlazorShop.Application.Commands.ClaimCommand
+{
+    /// <summary>
+    /// A model to update a claim.
+    /// </summary>
+    public class UpdateClaimCommand : IRequest<RequestResponse>
+    {
+        /// <summary>
+        /// Gets or sets the id of the claim.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value of the claim.
+        /// </summary>
+        public string Value { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of the claim.
+        /// </summary>
+        public string Type { get; set; }
+    }
+}
