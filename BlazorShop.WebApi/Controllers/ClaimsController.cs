388c8710dc01fd12382ed4713b6d34de9a3d357a
Jitca Beniamin <JBeni@users.noreply.github.com>
2023-09-18 22:23:59 +0100

    other changes

--- BlazorShop.WebApi/Controllers/ClaimsController.cs
+++ BlazorShop.WebApi/Controllers/ClaimsController.cs
@@ -1,31 +1,31 @@
-﻿// <copyright file="RolesController.cs" company="Beniamin Jitca">
+﻿// <copyright file="ClaimsController.cs" company="Beniamin Jitca">
 // Copyright (c) Beniamin Jitca. All rights reserved.
 // </copyright>
 
 namespace BlazorShop.WebApi.Controllers
 {
     /// <summary>
-    /// Controller for Roles.
+    /// Controller for Claims.
     /// </summary>
     [Authorize(Roles = $"{StringRoleResources.Admin}")]
-    public class RolesController : ApiBaseController
+    public class ClaimsController : ApiBaseController
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="RolesController"/> class.
+        /// Initializes a new instance of the <see cref="ClaimsController"/> class.
         /// </summary>
         /// <param name="mediator">The instance of the <see cref="IMediator"/> to use.</param>
-        public RolesController(IMediator mediator)
+        public ClaimsController(IMediator mediator)
             : base(mediator)
         {
         }
 
         /// <summary>
-        /// Create a role.
+        /// Create a Claim.
         /// </summary>
         /// <param name="command">The command.</param>
         /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
-        [HttpPost("role")]
-        public async Task<IActionResult> CreateRole([FromBody] CreateClaimCommand command)
+        [HttpPost("Claim")]
+        public async Task<IActionResult> CreateClaim([FromBody] CreateClaimCommand command)
         {
             var result = await this.Mediator.Send(command);
             return result.Successful == true
@@ -34,12 +34,12 @@ namespace BlazorShop.WebApi.Controllers
         }
 
         /// <summary>
-        /// Update a role.
+        /// Update a Claim.
         /// </summary>
         /// <param name="command">The command.</param>
         /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
-        [HttpPut("role")]
-        public async Task<IActionResult> UpdateRole([FromBody] UpdateClaimCommand command)
+        [HttpPut("Claim")]
+        public async Task<IActionResult> UpdateClaim([FromBody] UpdateClaimCommand command)
         {
             var result = await this.Mediator.Send(command);
             return result.Successful == true
@@ -48,12 +48,12 @@ namespace BlazorShop.WebApi.Controllers
         }
 
         /// <summary>
-        /// Delete a role.
+        /// Delete a Claim.
         /// </summary>
-        /// <param name="id">The id of the role.</param>
+        /// <param name="id">The id of the Claim.</param>
         /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
-        [HttpDelete("role/{id}")]
-        public async Task<IActionResult> DeleteRole(int id)
+        [HttpDelete("Claim/{id}")]
+        public async Task<IActionResult> DeleteClaim(int id)
         {
             var result = await this.Mediator.Send(new DeleteClaimCommand { Id = id });
             return result.Successful == true
@@ -62,12 +62,12 @@ namespace BlazorShop.WebApi.Controllers
         }
 
         /// <summary>
-        /// Get the role.
+        /// Get the Claim.
         /// </summary>
-        /// <param name="id">The id of the role.</param>
+        /// <param name="id">The id of the Claim.</param>
         /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
-        [HttpGet("role/{id}")]
-        public async Task<IActionResult> GetRoleById(int id)
+        [HttpGet("Claim/{id}")]
+        public async Task<IActionResult> GetClaimById(int id)
         {
             var result = await this.Mediator.Send(new GetClaimByIdQuery { Id = id });
             return result.Successful == true
@@ -76,26 +76,13 @@ namespace BlazorShop.WebApi.Controllers
         }
 
         /// <summary>
-        /// Get the user roles for a non admin role.
+        /// Get the user Claims for a non admin Claim.
         /// </summary>
         /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
-        [HttpGet("roles")]
-        public async Task<IActionResult> GetRoles()
+        [HttpGet("Claims")]
+        public async Task<IActionResult> GetClaims()
         {
             var result = await this.Mediator.Send(new GetClaimsQuery { });
-            return result.Successful == true
-                ? this.Ok(result)
-                : this.BadRequest(result);
-        }
-
-        /// <summary>
-        /// Get the roles for an admin user.
-        /// </summary>
-        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
-        [HttpGet("rolesAdmin")]
-        public async Task<IActionResult> GetRolesForAdmin()
-        {
-            var result = await this.Mediator.Send(new GetClaimByTypeQuery { });
             return result.Successful == true
                 ? this.Ok(result)
                 : this.BadRequest(result);
